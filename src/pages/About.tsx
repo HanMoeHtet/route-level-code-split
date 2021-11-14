dd884c8c95874d1b75090e516f45efcf39b8f4fe
HanMoeHtet <hapo23lert@gmail.com>
2021-11-14 11:10:47 +0630

    update

---
+++ src/pages/About.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+interface AboutProps {}
+
+const About: React.FC<AboutProps> = () => {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+interface HomeProps {}
+
+const Home: React.FC<HomeProps> = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
