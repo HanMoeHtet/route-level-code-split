a516f945e2e7b1f5009106459e809ef80047859d
HanMoeHtet <hapo23lert@gmail.com>
2021-11-14 11:12:38 +0630

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
 import Home from './pages/Home';
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
